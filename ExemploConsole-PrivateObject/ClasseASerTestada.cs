b3fbaba81f949263451b3db24d9899235b19975f
andrealveslima <andrealveslimeira@gmail.com>
2013-12-17 21:11:26 +0100

    Commit do exemplo

---
+++ ExemploConsole-PrivateObject/ClasseASerTestada.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploConsole_PrivateObject
+{
+    public class ClasseASerTestada
+    {
+        private bool MetodoPrivado()
+        {
+            return true;
+        }
+        private bool MetodoPrivado(bool parametro)
+        {
+            return parametro;
+        }
+        private string _atributoPrivado = "Teste";
+        public int PropriedadeComSetterPrivado { get; private set; }
+        private static string _atributoEstaticoPrivado = "Teste estático";
+        private static bool MetodoEstaticoPrivado()
+        {
+            return false;
+        }
+    }
+}
---
+++ ProjetoDeUnitTests/ClasseASerTestadaTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjetoDeUnitTests
+{
+    [TestClass]
+    public class ClasseASerTestadaTest
+    {
+        [TestMethod]
+        public void TesteDaAPIPrivada()
+        {
+            ExemploConsole_PrivateObject.ClasseASerTestada obj = new ExemploConsole_PrivateObject.ClasseASerTestada();
+            PrivateObject privateObj = new PrivateObject(obj);
+
+            // Testando MetodoPrivado.
+            var retorno = (bool)privateObj.Invoke("MetodoPrivado");
+            Assert.IsTrue(retorno);
+
+            // Testando MetodoPrivado com parâmetro.
+            retorno = (bool)privateObj.Invoke("MetodoPrivado", false);
+            Assert.IsFalse(retorno);
+
+            // Testando _atributoPrivado.
+            Assert.AreEqual("Teste", privateObj.GetField("_atributoPrivado"));
+            privateObj.SetField("_atributoPrivado", "Novo valor");
+            Assert.AreEqual("Novo valor", privateObj.GetField("_atributoPrivado"));
+
+            // Testando PropriedadeComSetterPrivado.
+            privateObj.SetProperty("PropriedadeComSetterPrivado", 51);
+            Assert.AreEqual(51, obj.PropriedadeComSetterPrivado);
+
+            PrivateType privateType = new PrivateType(typeof(ExemploConsole_PrivateObject.ClasseASerTestada));
+
+            // Testando _atributoEstaticoPrivado.
+            Assert.AreEqual("Teste estático", privateType.GetStaticField("_atributoEstaticoPrivado"));
+            privateType.SetStaticField("_atributoEstaticoPrivado", "Novo valor estático");
+            Assert.AreEqual("Novo valor estático", privateType.GetStaticField("_atributoEstaticoPrivado"));
+
+            // Testando MetodoEstaticoPrivado.
+            retorno = (bool)privateType.InvokeStatic("MetodoEstaticoPrivado");
+            Assert.IsFalse(retorno);
+        }
+    }
+}
